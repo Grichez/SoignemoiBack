2d138059864e5e7209c23c6281e4724acbc29da0
Geoffroy Richez <g.richez@gmail.com>
2024-03-06 20:56:56 +0100

    implements gitignore

---
+++ src/test/java/com/geodev/soignemoiapp/SoignemoiappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geodev.soignemoiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoignemoiappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
